55f7230fd5c61476b909adb4671a746598fc4969
Inzmam Khan <khaninzmam59@gmail.com>
2026-01-24 00:05:25 +0530

    Add files via upload

---
+++ Custom Module/Module_MyCustom.py
@@ -0,0 +1,14 @@
+#   A MODULE is a File Containing Code you Want to Include in your Program
+
+def greet(name):
+    result = f"What's Up, {name}"
+    return result
+
+@staticmethod
+def starteating():
+    result = f"Let's Begin"
+    return result
+
+def leave(name):
+    result =f"Sayonara {name}"
+    return result
---
+++ Custom Module/Module_Test Run Program.py
@@ -0,0 +1,10 @@
+import Module_MyCustom as Mm
+
+result1 = Mm.greet("Phoenix")
+print(result1)
+
+result2 = Mm.starteating()
+print(result2)
+
+result3 = Mm.leave("Phoenix")
+print(result3)
