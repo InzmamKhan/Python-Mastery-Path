b7d809410cbb17660fd9ed8198d934184e296e06
Inzmam Khan <khaninzmam59@gmail.com>
2026-01-26 00:42:17 +0530

    Add files via upload

---
+++ Pandas/1. Importing & Reading Files.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Loading Files
+pokeData = pd.read_csv("C:\\Users\\khani\\OneDrive\\Desktop\\Practice Code\\PYTHON\\Databases\\Pandas\\Source File - Kanto_Region.csv")
+print("My PokeData :")
+print(pokeData)
+print()
