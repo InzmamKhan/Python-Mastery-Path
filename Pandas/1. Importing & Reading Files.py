f5584737c6f449c9b90434feaea8065f69307201
Inzmam Khan <khaninzmam59@gmail.com>
2026-01-26 00:45:42 +0530

    Delete Pandas directory

--- Pandas/1. Importing & Reading Files.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Loading Files
-pokeData = pd.read_csv("C:\\Users\\khani\\OneDrive\\Desktop\\Practice Code\\PYTHON\\Databases\\Pandas\\Source File - Kanto_Region.csv")
-print("My PokeData :")
-print(pokeData)
-print()
